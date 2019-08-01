b20e3d1e496b3705378122cb049ee56798fe2316
Leon <liang.li@charleskeith.com>
2019-08-01 18:47:48 +0800

    create podSpec

---
+++ PodTest/PodTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodTest
+//
+//  Created by keith on 2019/8/1.
+//  Copyright © 2019 Charles&Keith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
